3f6133ae7eeda3682b46d965d76776609c548521
MCydia <61176750+MCydia@users.noreply.github.com>
2021-06-13 10:16:20 +0800

    Update openwrt-21.02.sh

--- openwrt-21.02.sh
+++ openwrt-21.02.sh
@@ -4,6 +4,10 @@ svn co https://github.com/tuanqing/install-program/trunk ./install-program
 svn co https://github.com/281677160/openwrt-package/branches/tladg/luci-app-adguardhome
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ttyd
 
+curl -fsSL  https://raw.githubusercontent.com/281677160/openwrt-package/usb/argon/footer.htm > ./luci-theme-argon/luasrc/view/themes/argon/footer.htm
+sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
+sed -i "s/bing_background '0'/bing_background '1'/g" ./luci-app-argon-config/root/etc/config/argon
+
 rm -rf ./*/.git
 rm -rf ./*/.svn
 rm -rf ./*/*/.svn
