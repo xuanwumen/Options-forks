45c3e41d57e0838af1c44163b222fbc5b4107107
MCydia <61176750+MCydia@users.noreply.github.com>
2021-06-13 10:15:33 +0800

    Update openwrt-21.02.sh

--- openwrt-21.02.sh
+++ openwrt-21.02.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+svn co https://github.com/tuanqing/install-program/trunk ./install-program
+svn co https://github.com/281677160/openwrt-package/branches/tladg/luci-app-adguardhome
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ttyd
+
 rm -rf ./*/.git
 rm -rf ./*/.svn
 rm -rf ./*/*/.svn
