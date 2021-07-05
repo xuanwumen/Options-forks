02413c84d62441d1745accccdca9a5bc13db0f72
MCydia <61176750+MCydia@users.noreply.github.com>
2021-07-05 21:30:12 +0800

    Update and rename openwrt-18.06-k5.4.sh to kernel.sh

---
+++ kernel.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+svn co https://github.com/tuanqing/mknop/trunk ./
+
+# 生成完整目录清单
+cat >> Update.md <<EOF
+kernel
+EOF
+
+# 获取所有更新目录并显示
+ls | grep -v 'Update.md' | grep -v 'main.sh' >> UpdateList.md
+
+# 对比Update.md文件里没有的内容，并生成变量
+echo 缺失包列表
+FOLDERS=`grep -Fxvf UpdateList.md Update.md`
+FOLDERSX=`echo $FOLDERS | sed 's/ /、/g'`;echo $FOLDERSX
+
+# 判断变量值，如果有效发送微信通知
+# if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=$FOLDERSX--同步失败; fi
+# 删除对比更新目录列表
+rm -rf UpdateList.md
+rm -rf Update.md
+
+
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./*/*/.svn
+rm -rf ./*/*/.git
+rm -rf ./.github
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
